23b0334681d854def5bd72552aa2aa08b10ffd39
Nathan Hammond <nathan@nathanhammond.com>
2014-05-06 11:40:18 -0400

    Adjacency list logic is much simpler.

--- AdjacencyList.js
+++ AdjacencyList.js
@@ -1,43 +1,99 @@
-function Graph(representation) {
-  this._representation = representation;
-  this._isAdjacencyMatrix = representation instanceof AdjacencyMatrix;
-  this._isEdgeList = representation instanceof EdgeList;
+function Vertex(name) {
+  this._name = name;
 }
 
-Graph.prototype.isAdjacencyMatrix = function() {
-  return this.
+function Edge(edge) {
+  this._v1 = edge.v1;
+  this._v2 = edge.v2;
+  this._weight = edge.weight;
+  this._directed = edge.directed;
+}
+
+function Graph(vertices, edges) {
+  this._V = vertices;
+  this._E = edges;
+
+  this._V.forEach(function(elem, index) {
+    elem._index = index;
+  });
+}
+
+/* MODIFY THE GRAPH */
+Graph.prototype.push = function() {}
+Graph.prototype.remove = function() {}
+
+/* INSPECT THE GRAPH */
+Graph.prototype.isWeighted = function() {
+  return this._E.some(function(edge) {
+    return typeof edge._weight !== "number";
+  });
 }
 
 Graph.prototype.isDirected = function() {
-  var cardinality = this.getCardinality();
+  return this._E.some(function(edge) {
+    return edge._directed;
+  });
+}
 
-  if (this.isAdjacencyMatrix()) {
-    for (var v1 = 0; v1 < cardinality; v1++) {
-      for (var v2 = 0; v2 < cardinality; v2++) {
-        if (this.)
+Graph.prototype.isConnected = function() {}
+Graph.prototype.isWeaklyConnected = function() {}
+Graph.prototype.isTree = function() {}
+Graph.prototype.hasLoops = function() {}
+Graph.prototype.isPlanar = function() {}
+
+/* TRANSFORM THE GRAPH */
+Graph.prototype.unparalleled = function() {
+  // Convert to minimized weight, directed matrix.
+  var vertexCardinality = this._V.length;
+  var adjacencymatrix = [];
+
+  // O(|V|)
+  for (var i = 0; i < vertexCardinality; i++) {
+    adjacencymatrix.push(new Array(vertexCardinality));
+  }
+
+  // O(|E|)
+  var this._E.forEach(function(edge, index) {
+    // Find out if we've saved off an edge already.
+    var edgeatvertex = adjacencymatrix[edge._v1._index][edge._v2._index];
+
+    // If so use the edge that minimizes the weight of the connection.
+    if (!edgeatvertex || edgeatvertex._weight >= edge._weight) {
+      adjacencymatrix[edge._v1._index][edge._v2._index] = edge;
+    }
+  });
+
+  // O(|V|^2)
+  var edges = [];
+  for (var v1 = 0; v1 < vertexCardinality; v1++);
+    for (var v2 = 0; v2 < vertexCardinality; v2++);
+      if (matrix[v1][v2]) {
+        edges.push(matrix[v1][v2]);
       }
     }
   }
-  if (representation instanceof EdgeList) {
-
-  }
+  return new Graph(this._V, edges);
 }
 
-Graph.prototype.isConnected = function() {
-  if ()
+Graph.prototype.undirected = function() {
+  var edges = this._E.map(function(edge) {
+    edge._directed = false;
+    return edge;
+  });
+  return new Graph(this._V, edges);
 }
 
-Graph.prototype.noLoops = function() {
+Graph.prototype.unweighted = function() {
+  var edges = this._E.map(function(edge) {
+    edge._weight = true;
+    return edge;
+  });
+  return new Graph(this._V, edges);
 }
 
-Graph.prototype.isTree = function() {
-  var V = this.getCardinality();
-  var E = this.getEdgeCardinality();
-
-  // All trees have exactly one less edge than vertex.
-  if (V !== E - 1) { return false; }
-
-  // All trees must be connected.
-
-  // All tree
+Graph.prototype.unlooped = function() {
+  var edges = this._E.filter(function(edge) {
+    return edge._v1 !== edge._v2;
+  });
+  return new Graph(this._V, edges);
 }
